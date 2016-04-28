cbf2387eec59ddb24fbf38e3a9e07eaa9eb29572
Yan Kurobane <yan.kurobane@gmail.com>
2016-04-28 20:25:06 +0300

    Setup ArrayAdapter, now shows scrollable list of Strings

--- app/src/main/java/com/kurobane/yan/tasqr/MainActivityFragment.java
+++ app/src/main/java/com/kurobane/yan/tasqr/MainActivityFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -32,6 +33,13 @@ public class MainActivityFragment extends Fragment {
                 "Thurs 6/26 - Rainy - 18/11",
                 "Fri 6/27 - Foggy - 21/10",
                 "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Sun 6/29 - Sunny - 20/7",
+                "Mon 6/23 - Sunny - 31/17",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
                 "Sun 6/29 - Sunny - 20/7"
         };
 
@@ -44,6 +52,11 @@ public class MainActivityFragment extends Fragment {
                 weekForecast
         );
 
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_task);
+        listView.setAdapter(mTaskAdapter);
+
+        return rootView;
     }
 }
